5c35cecd89199cb3aa5fe2fd1d27a74ca435460b
anna_pilipenko <hanna.pilipenko@gmail.com>
2018-10-06 06:27:25 +0300

    first commit

---
+++ 3DProjection/Models/3DObject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DProjection.Models
+{
+    class _3DObject
+    {
+        private List<Node> Nodes { get; set; } = new List<Node>();
+
+        public List<Node> GetOZProjection()
+        {
+            List<Node> nodes = new List<Node>();
+            foreach (Node node in this.Nodes)
+            {
+                nodes.Add(NodeFactory.GetNode2D(node.X, node.Z));
+            }
+
+            return nodes;
+        }
+    }
+}
---
+++ 3DProjection/Models/Node.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3DProjection.Models
+{
+    public class Node
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+        public List<Node> NeighborNodes { get; private set; }
+
+        internal Node(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+            this.NeighborNodes = new List<Node>();
+        }
+
+        public void TryAddNeighborNode(double x, double y, double z)
+        {
+            var node = new Node(x, y, z);            
+            this.TryAddNeighborNode(node);
+        }
+
+        public void TryAddNeighborNode(Node node)
+        {
+            if (!this.NeighborNodes.Contains(node))
+            {
+                this.NeighborNodes.Add(node);
+
+                if (!node.NeighborNodes.Contains(this))
+                {
+                    node.NeighborNodes.Add(this);
+                }
+            }
+            else
+            {
+                throw new Exception("The node already contains this neighbor.");
+            }
+        }
+    }
+}
