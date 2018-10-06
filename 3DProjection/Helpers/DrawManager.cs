a9bfcb0b80e303657255bae586a7b80832e68296
anna_pilipenko <hanna.pilipenko@gmail.com>
2018-10-06 09:06:50 +0300

    add drawing lines

--- 3DProjection/Helpers/DrawManager.cs
+++ 3DProjection/Helpers/DrawManager.cs
@@ -1,5 +1,7 @@
 ﻿using _3DProjection.Models;
+using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Shapes;
 
@@ -9,6 +11,12 @@ namespace _3DProjection.Helpers
     {
         private Canvas canvas;
         private Object3D object3d;
+
+        private Point startDrawingMousePosition;
+        private bool isDrawingLine = false;
+        private Line currentDrawingLine;
+
+        private Brush brush = (Brush)(new System.Windows.Media.BrushConverter()).ConvertFromString("#4527a0");
 
         private DrawManager()
         {
@@ -33,6 +41,9 @@ namespace _3DProjection.Helpers
         {
             this.canvas = canv;
             this.object3d = new Object3D();
+            
+            this.canvas.AddHandler(FrameworkElement.MouseDownEvent, new MouseButtonEventHandler(this.Canvas_MouseLeftButtonDown), true);
+            this.canvas.MouseMove += this.Canvas_MouseMove;
         }
 
         public void AddNode(double x, double y, double z)
@@ -45,9 +56,9 @@ namespace _3DProjection.Helpers
         {
             Ellipse circle = new Ellipse()
             {
-                Width = 10,
-                Height = 10,
-                Fill = (Brush)(new System.Windows.Media.BrushConverter()).ConvertFromString("#4527a0"),
+                Width = 8,
+                Height = 8,
+                Fill = this.brush,
                 StrokeThickness = 0
             };
 
@@ -56,6 +67,61 @@ namespace _3DProjection.Helpers
             circle.SetValue(Canvas.LeftProperty, (double)node.X);
             circle.SetValue(Canvas.TopProperty, (double)node.Z);
         }
+
+        private void DrawLine(double x1, double y1, double x2, double y2)
+        {
+            Line line = new Line();
+            this.currentDrawingLine = line;
+            line.Stroke = this.brush;
+            line.X1 = x1;
+            line.Y1 = y1;
+            line.X2 = x2;
+            line.Y2 = y2;
+
+            line.StrokeThickness = 2;
+            this.canvas.Children.Add(line);
+        }
+
+        private void Canvas_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            IInputElement clickedElement = Mouse.DirectlyOver;
+            if (!this.isDrawingLine)
+            {
+                if (clickedElement is Ellipse)
+                {
+                    this.isDrawingLine = true;
+                    this.startDrawingMousePosition = e.GetPosition((Canvas)sender);
+                }
+            }
+            else
+            {
+                this.isDrawingLine = false;
+                if (clickedElement is Ellipse && this.currentDrawingLine != null)
+                {
+                    this.canvas.Children.Remove(this.currentDrawingLine);
+                    this.DrawLine(this.startDrawingMousePosition.X, this.startDrawingMousePosition.Y, e.GetPosition((Canvas)sender).X, e.GetPosition((Canvas)sender).Y);
+                    this.currentDrawingLine = null;
+                }
+                else if (this.currentDrawingLine != null)
+                {
+                    this.canvas.Children.Remove(this.currentDrawingLine);
+                }
+            }
+        }
+
+        private void Canvas_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (this.isDrawingLine)
+            {
+                if (this.currentDrawingLine != null)
+                {
+                    this.canvas.Children.Remove(this.currentDrawingLine);
+                }
+
+                this.DrawLine(this.startDrawingMousePosition.X, this.startDrawingMousePosition.Y, e.GetPosition((Canvas)sender).X, e.GetPosition((Canvas)sender).Y - 10);
+            }
+        }
+
 
     }
 }
