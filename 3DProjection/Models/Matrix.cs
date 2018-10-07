8636ea5ba218b250c02e0576a1298c42ebf55310
anna_pilipenko <hanna.pilipenko@gmail.com>
2018-10-07 09:58:50 +0300

    add multiplying matrix by vector

--- 3DProjection/Models/Matrix.cs
+++ 3DProjection/Models/Matrix.cs
@@ -8,24 +8,6 @@ namespace _3DProjection.Models
 {
     public class Matrix
     {
-        public static int[,] Multiply(int[,] a, int[,] b)
-        {
-            if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
-
-            int[,] result = new int[a.GetLength(0), b.GetLength(1)];
-            for (int i = 0; i < a.GetLength(0); i++)
-            {
-                for (int j = 0; j < b.GetLength(1); j++)
-                {
-                    for (int k = 0; k < b.GetLength(0); k++)
-                    {
-                        result[i, j] += a[i, k] * b[k, j];
-                    }
-                }
-            }
-            return result;
-        }
-
         public static double[,] Multiply(double[,] a, double[,] b)
         {
             if (a.GetLength(1) != b.GetLength(0)) throw new Exception("Матрицы нельзя перемножить");
@@ -42,6 +24,24 @@ namespace _3DProjection.Models
                 }
             }
             return result;
+        }
+
+        public static double[] Multiply(double[] a, double[,] b)
+        {
+            double[,] c = new double[a.Length, 1];
+            for (int i = 0; i < a.Length; i++)
+            {
+                c[i, 0] = a[i];
+            }
+
+            double[,] result = Matrix.Multiply(c, b);
+            double[] newResult = new double[result.GetLength(0)];
+            for (int i = 0; i < result.GetLength(0); i++)
+            {
+                newResult[i] = result[i, 0];
+            }
+
+            return newResult;
         }
     }
 }
